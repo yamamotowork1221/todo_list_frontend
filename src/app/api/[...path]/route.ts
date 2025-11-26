18888cd4c5b8d8c41691bd006829fa452e358a56
yamamotowork1221 <yamamotowork1221@gmail.com>
2025-11-26 14:07:54 +0900

    Safari countermeasure

--- src/app/api/[...path]/route.ts
+++ src/app/api/[...path]/route.ts
@@ -4,17 +4,20 @@ import { env } from '@/config/env';
 const BACKEND_URL = env.BACKEND_URL
 
 export async function handler(req: NextRequest) {
-  const url = `${BACKEND_URL}${req.nextUrl.pathname}`
+    const url = `${BACKEND_URL}${req.nextUrl.pathname}`
 
-  const fetchOptions: RequestInit = {
-    method: req.method,
-    headers: Object.fromEntries(req.headers.entries()),
-    body: ['GET', 'HEAD'].includes(req.method ?? '') ? undefined : await req.text()
-  }
+    const fetchOptions: RequestInit = {
+        method: req.method,
+        headers: {
+            ...Object.fromEntries(req.headers.entries()),
+        },
+        body: ['GET', 'HEAD'].includes(req.method ?? '') ? undefined : await req.text(),
+        credentials: 'include'
+    }
 
-  const res = await fetch(url, fetchOptions)
-  const data = await res.text()
-  return new NextResponse(data, { status: res.status })
+    const res = await fetch(url, fetchOptions)
+    const data = await res.text()
+    return new NextResponse(data, { status: res.status })
 }
 
 export const GET = handler
