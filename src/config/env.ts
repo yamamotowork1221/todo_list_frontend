0d6ff08173e54b1f013d049bbf9479f935cbe1c5
yamamotowork1221 <yamamotowork1221@gmail.com>
2025-11-26 13:50:06 +0900

    Safari countermeasure

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,5 +5,5 @@ interface Env {
 
 export const env: Env = {
     BACKEND_ADDRESS: process.env.NEXT_PUBLIC_BACKEND_ADDRESS || 'https://',
-    BACKEND_URL: process.env.NEXT_PUBLIC_BBACKEND_URL || 'https://',
+    BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://',
 } as const;
